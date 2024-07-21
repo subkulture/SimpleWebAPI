27af84139d05bf01c10bd33bf434f76a6c898648
Shane <shane_lynott@hotmail.co.uk>
2024-07-21 17:40:39 +0100

    added method to customer

--- src/SimpleWebAPI.Domain/Customers/Customer.cs
+++ src/SimpleWebAPI.Domain/Customers/Customer.cs
@@ -3,9 +3,14 @@
     public class Customer
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string Surname { get; set; }
-        public string Email { get; set; }
-        public string MobileNumber { get; set; }
+        public string? FirstName { get; set; }
+        public string? Surname { get; set; }
+        public string? Email { get; set; }
+        public string? MobileNumber { get; set; }
+
+        private bool IsEmailValid()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
